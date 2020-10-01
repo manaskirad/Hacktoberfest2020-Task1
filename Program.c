586d914dcba3083509c6b53bc39a48eeff18e75b
manaskirad <71597978+manaskirad@users.noreply.github.com>
2020-10-01 19:05:27 +0530

    updated c program
    1]Age was not described in main
    2]scanf needed at age

--- Program.c
+++ Program.c
@@ -2,15 +2,15 @@
 
 int main()
 {
-    char name,country,skill;
+    char name,country,skill,age;
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
-    scanf("%d",&name);
-    printf("Enter your Country of Residance : ");//Get user country
+    scanf("%s",&name);
+    printf("Enter your Country of Residance :\n ");//Get user country
     scanf("%s",&country);
-    printf("Enter your Age : ");//Get user Age
-    printf("%d",&age);
-    printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user skill level
+    printf("Enter your Age :\n ");//Get user Age
+    scanf("%d",&age);
+    printf("Rate your programmings skills from A-D (A being the highest and D being the lowest :\n ");//Get user skill level
     scanf(" %c",&skill);
     scanf("Thank you %c Happy Hacking",country);//Thank the user
 
